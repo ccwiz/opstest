abaf6892891e19c4e353b535c6801e8dee04eac3
ccwiz <ville.salkala@sap.com>
2015-12-07 16:19:07 +0200

    testing more

--- recipes/addtenant.rb
+++ recipes/addtenant.rb
@@ -1,10 +1,17 @@
 Chef::Log.info("CUSTOM PARAMS  tenantid: #{node['tenantid']} agentIP #{node['agent_ip']} visitorIP #{node['visitor_ip']} RCI_IP #{node['rci_ip']}")
 
-cookbook_file "/tmp/test.py" do
-  source "test.py"
-  mode 0755
-end
+tenantid = node['tenantid']
+agentip = node['agent_ip']
+visitorip = node['visitor_ip']
+rciip = node['rci_ip']
 
-execute "install my lib" do
-  command "sudo python /tmp/test.py"
+if tenantid && agentip && vistiorip && rciip
+	cookbook_file "/tmp/test.py" do
+	  source "test.py"
+	  mode 0755
+	end
+
+	execute "install my lib" do
+	  command "sudo python /tmp/test.py #{node['tenantid']}"
+	end
 end
